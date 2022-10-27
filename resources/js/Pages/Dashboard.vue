08f377f1ec3aa0eda7b8cf4de06eeda799f7c774
Coen <coenonderwater@gmail.com>
2022-10-27 23:28:46 +0200

    feat(strava-auth): add link to to authorize strava

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,9 +15,13 @@ import { Head } from '@inertiajs/inertia-vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in change woah!!
+                        You're logged in change!!
+                        {{ $page.props.stravaAuthUrl }}
+                    </div>
+                    <div class="p-6 mt-6 bg-white border-b border-gray-200">
+                        <a :href=$page.props.stravaAuthUrl class="underline">Connect TotalCaliber to your strava</a>
                     </div>
                 </div>
             </div>
