7f6446d79e917ccbf5f8b0c8849e77c44d5d003c
Coen <coenonderwater@gmail.com>
2022-10-29 00:22:06 +0200

    feat(strava-auth): store the auth token for strava

--- app/Http/Controllers/Auth/StravaAuthorizeController.php
+++ app/Http/Controllers/Auth/StravaAuthorizeController.php
@@ -3,21 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\StravaAuthToken;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class StravaAuthorizeController extends Controller
 {
-    public function authorized(Request $request)
+    public function authorized(Request $request): Response
     {
-      $user = $request->user();
-      \Log::info($user);
-      \Log::info($request->all());
-
-      // array (
-      //   'state' => NULL,
-      //   'code' => '656c164a168133bb414deded4ab821c18b24f1a6',
-      //   'scope' => 'read,activity:write,activity:read_all',
-      // )  
+        $user = $request->user();
+        
+        StravaAuthToken::updateOrCreate([
+            'user_id' => $user->id,
+            'code' => $request->get('code'),
+            'scopes' => $request->get('scope')
+        ]);
+ 
+        return Inertia::render('Dashboard', ['success_message' => 'Strava succesfully authenticated']);
     }
-
 }
