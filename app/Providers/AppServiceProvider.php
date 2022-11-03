8fabe302717d59ea640832690a6d61144678d9b1
Coen <coenonderwater@gmail.com>
2022-11-03 22:07:05 +0100

    chore: register strava client as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\StravaClient;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(StravaClient::class, function ($app) {
+            return new StravaClient();
+        });
     }
 }
