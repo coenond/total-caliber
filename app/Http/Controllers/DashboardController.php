d9dcd43a38095cc4d87401abd3e84da10018bc83
Coen <coenonderwater@gmail.com>
2022-11-03 22:06:27 +0100

    feat(strava-auth): pass message to FE

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\StravaAuthService;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -14,8 +15,11 @@ class DashboardController extends Controller
         private StravaAuthService $stravaAuthService
     ) { }
 
-    public function renderDashboardPage(): Response
+    public function renderDashboardPage(Request $req): Response
     {
-        return Inertia::render('Dashboard', ['stravaAuthUrl' => $this->stravaAuthService->getAuthorizationUrl()]);
+        return Inertia::render('Dashboard', [
+            'stravaAuthUrl' => $this->stravaAuthService->getAuthorizationUrl(),
+            'success_message' => $req->has('success_message') ? $req->input('success_message') : null
+        ]);
     }
 }
