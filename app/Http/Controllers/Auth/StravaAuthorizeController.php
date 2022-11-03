16644cdd28ff83fec1ba22fabbb511fe09f2fbc2
Coen <coenonderwater@gmail.com>
2022-11-03 22:06:17 +0100

    feat(strava-auth):add message when auth was successfull

--- app/Http/Controllers/Auth/StravaAuthorizeController.php
+++ app/Http/Controllers/Auth/StravaAuthorizeController.php
@@ -4,21 +4,26 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\StravaAuthToken;
+use App\Services\StravaAuthService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class StravaAuthorizeController extends Controller
 {
-    public function authorized(Request $request): Response
+    public function __construct(
+        private StravaAuthService $stravaAuthService
+    ) { }
+
+    public function authorized(Request $req): Response
     {
-        $user = $request->user();
-        
-        StravaAuthToken::updateOrCreate([
-            'user_id' => $user->id,
-            'code' => $request->get('code'),
-            'scopes' => $request->get('scope')
-        ]);
+        $user = $req->user();
+        /** @var StravaAuthToken */
+        $stravaAuthToken = $this->stravaAuthService->storeUserStravaCode(
+            $user, $req->get('code'), $req->get('scope')
+        );
+
+        $this->stravaAuthService->setUserInitialRefreshToken($stravaAuthToken);
  
         return Inertia::render('Dashboard', ['success_message' => 'Strava succesfully authenticated']);
     }
