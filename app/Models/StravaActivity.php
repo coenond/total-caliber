58f95377650f2494162fac824964f8a58e6541d8
Coen <coenonderwater@gmail.com>
2022-11-16 23:40:08 +0100

    feat(strava-sync): add readable attributes

--- app/Models/StravaActivity.php
+++ app/Models/StravaActivity.php
@@ -9,5 +9,30 @@ class StravaActivity extends Model
 {
     use HasFactory;
 
+    private const KM_TO_MILES_RATE = 0.62137;
+
     protected $guarded = ['id'];
+    protected $appends = ['readableDistanceInKm', 'readableDistanceInMiles', 'readableTime'];
+
+    protected function getReadableDistanceInKmAttribute(): string
+    {
+        if ($this->distance === 0.00) {
+            return '---';
+        }
+        return number_format($this->distance / 1_000, 2) . ' km';
+    }
+
+    public function getReadableDistanceInMilesAttribute(): string
+    {
+        if ($this->distance === 0.00) {
+            return '---';
+        }
+        return number_format(($this->distance / 1_000) * self::KM_TO_MILES_RATE, 2) . ' miles';
+    }
+
+    public function getReadableTimeAttribute(): string
+    {
+        $s = $this->moving_time;
+        return sprintf('%02d:%02d:%02d', ($s / 3_600), ($s / 60 % 60), $s % 60);
+    }
 }
