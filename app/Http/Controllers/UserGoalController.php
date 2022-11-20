dc832e56652012e9ae63c4f3aa4cc049e96ce6c0
Coen <coenonderwater@gmail.com>
2022-11-20 23:55:24 +0100

    feat(user-goals): store the goal in the database

--- app/Http/Controllers/UserGoalController.php
+++ app/Http/Controllers/UserGoalController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserGoalRequest;
+use App\Models\UserGoal;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +11,19 @@ class UserGoalController extends Controller
 {
     public function index()
     {
+        return Inertia::render('UserGoalOverview');
+    }
+
+    public function store(StoreUserGoalRequest $req)
+    {
+        $user = $req->user();
+        // @todo: Move to service
+        UserGoal::create([
+            'user_id' => $user->id,
+            'name' => $req->name(),
+            'start' => $req->start(),
+            'end' => $req->end()
+        ]);
         return Inertia::render('UserGoalOverview');
     }
 }
