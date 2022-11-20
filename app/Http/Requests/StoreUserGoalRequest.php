761094626897cd0105dfa64a25e68072e94dfe64
Coen <coenonderwater@gmail.com>
2022-11-20 23:54:32 +0100

    feat(user-goals): add goal request class

---
+++ app/Http/Requests/StoreUserGoalRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
+
+class StoreUserGoalRequest extends FormRequest
+{
+    public function authorize() { return true; }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'goalTitle' => 'required|string',
+            'goalStart' => 'required|date',
+            'goalEnd' => 'required|date|after:goalStart'
+        ];
+    }
+
+    public function name(): string
+    {
+        return $this->string('goalTitle');
+    }
+
+    public function start(): Carbon
+    {
+        return $this->date('goalStart');
+    }
+
+    public function end(): Carbon
+    {
+        return $this->date('goalEnd');
+    }
+}
