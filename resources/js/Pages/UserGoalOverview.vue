d79703b9ca4fca4c9acf01b5bce542a74e9bf113
Coen <coenonderwater@gmail.com>
2022-11-21 22:17:16 +0100

    feat(user-goals): show the goal if there is alread 1 set

--- resources/js/Pages/UserGoalOverview.vue
+++ resources/js/Pages/UserGoalOverview.vue
@@ -18,11 +18,20 @@ import GoalInputForm from '@/Components/GoalInputForm.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-sm sm:rounded-lg p-6 bg-white border-b border-gray-200">
-                    <p>
-                        Set your goals to track your effords.
-                    </p>
+                    <div v-if="$page.props.userGoal !== null">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Your goal</h2>
+                        
+                        <p>name: <strong>{{ $page.props.userGoal.name }}</strong></p>
+                        <p>Goal Date: {{ $page.props.userGoal.end }}</p>
+                        <p>Started training at: {{ $page.props.userGoal.start }}</p>
 
-                    <GoalInputForm />
+                    </div>
+                    <div v-else>
+                        <p>
+                            Set your goals to track your effords.
+                        </p>
+                        <GoalInputForm />
+                    </div>
                 </div>
             </div>
         </div>
