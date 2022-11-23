f1e352f30cfc2828a3375619dfa934905bacd468
Coen <coenonderwater@gmail.com>
2022-11-24 00:00:51 +0100

    feat(webhooks): implement strava webhook verification

---
+++ app/Http/Controllers/StravaWebhookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class StravaWebhookController extends Controller
+{
+    public function handle(Request $req)
+    {
+        return new JsonResponse(['message' => 'ok']);
+    }
+
+    /**
+     * When subscribing to strava webhooks, Strava will verify the URL on this end-point
+     *
+     * @param Request $req
+     *
+     * @return void
+     */
+    public function verify(Request $req)
+    {
+        return new JsonResponse($req->only('hub_challenge'));
+    }
+}
