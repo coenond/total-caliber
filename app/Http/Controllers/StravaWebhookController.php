17b00b656f932c13a17228219fe73714319f878a
Coen <coenonderwater@gmail.com>
2022-11-24 00:27:22 +0100

    chore: temp write incoming strava webhooks to logs

--- app/Http/Controllers/StravaWebhookController.php
+++ app/Http/Controllers/StravaWebhookController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class StravaWebhookController extends Controller
 {
     public function handle(Request $req)
     {
+        Log::channel('strava_webhooks')->info('=========================');
+        Log::channel('strava_webhooks')->info(print_r($req->all(), true));
+        Log::channel('strava_webhooks')->info('=========================');
         return new JsonResponse(['message' => 'ok']);
     }
 
