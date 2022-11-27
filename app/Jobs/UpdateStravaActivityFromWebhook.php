5f8f675587104f6301f901ac0ffa133fffcf5336
Coen <coenonderwater@gmail.com>
2022-11-27 22:02:20 +0100

    feat(strava-sync): implement activity updates

--- app/Jobs/UpdateStravaActivityFromWebhook.php
+++ app/Jobs/UpdateStravaActivityFromWebhook.php
@@ -2,8 +2,9 @@
 
 namespace App\Jobs;
 
-use App\Models\User;
-use App\Services\StravaActivityService;
+use App\Models\StravaActivity;
+use App\Models\StravaProfile;
+use App\Models\StravaSportType;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -22,7 +23,33 @@ class UpdateStravaActivityFromWebhook implements ShouldQueue
         private array $updates
     ) { }
 
-    public function handle(StravaActivityService $activityService): void
+    public function handle(): void
     {
+        $user = StravaProfile::whereStravaId($this->athleteId)->firstOrFail()->user;
+
+        $activity = StravaActivity::whereStravaId($this->activityId)
+            ->whereUserId($user->id)
+            ->findOrFail();
+
+        if (array_key_exists('title', $this->updates)) {
+            $this->updateTitle($activity);
+        }
+
+        if (array_key_exists('type', $this->updates)) {
+            $this->updateType($activity);
+        }
+
+        $activity->save();
+    }
+
+    private function updateTitle(StravaActivity &$activity): void
+    {
+        $activity->title = $this->updates['title'];
+    }
+
+    private function updateType(StravaActivity &$activity): void
+    {
+        $newType = StravaSportType::whereType($this->updates['type'])->firstOrFail();
+        $activity->type_id = $newType->id;
     }
 }
