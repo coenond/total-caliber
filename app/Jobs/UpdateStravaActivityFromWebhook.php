027b575f7b9628e74f65f3230acd115dcdfd72da
Coen <coenonderwater@gmail.com>
2022-11-27 22:38:49 +0100

    refactor: fetch activity from strava when type changes

--- app/Jobs/UpdateStravaActivityFromWebhook.php
+++ app/Jobs/UpdateStravaActivityFromWebhook.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\StravaActivity;
 use App\Models\StravaProfile;
 use App\Models\StravaSportType;
+use App\Services\StravaActivityService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -13,8 +14,6 @@ use Illuminate\Queue\SerializesModels;
 
 class UpdateStravaActivityFromWebhook implements ShouldQueue
 {
-    private const PAGE_LIMIT = 100;
-
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function __construct(
@@ -23,33 +22,30 @@ class UpdateStravaActivityFromWebhook implements ShouldQueue
         private array $updates
     ) { }
 
-    public function handle(): void
+    public function handle(StravaActivityService $activityService): void
     {
         $stravaProfile = StravaProfile::whereStravaId($this->athleteId)->firstOrFail();
 
+        if (array_key_exists('title', $this->updates)) {
+            $this->updateTitle($stravaProfile);
+        }
+
+        /**
+         * The webhook doesn't give the dedicated "sport type". So activity has to be fetched from
+         * Strava to update to the right type. 
+         */
+        if (array_key_exists('type', $this->updates)) {
+            $user = $stravaProfile->user;
+            $activityService->getOneFromStravaAndStore($user, $this->activityId);
+        }
+    }
+
+    private function updateTitle(StravaProfile $stravaProfile): void
+    {
         $activity = StravaActivity::whereStravaId($this->activityId)
             ->whereUserId($stravaProfile->user_id)
             ->firstOrFail();
-
-        if (array_key_exists('title', $this->updates)) {
-            $this->updateTitle($activity);
-        }
-
-        if (array_key_exists('type', $this->updates)) {
-            $this->updateType($activity);
-        }
-
-        $activity->save();
-    }
-
-    private function updateTitle(StravaActivity &$activity): void
-    {
         $activity->name = $this->updates['title'];
-    }
-
-    private function updateType(StravaActivity &$activity): void
-    {
-        $newType = StravaSportType::whereType($this->updates['type'])->firstOrFail();
-        $activity->type_id = $newType->id;
+        $activity->save();
     }
 }
