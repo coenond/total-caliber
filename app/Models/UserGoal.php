98baa6f4c959b14194f5b749abb5428d0aa8d195
Coen <coenonderwater@gmail.com>
2022-11-30 22:56:33 +0100

    chore: add missing sportTypes relation

--- app/Models/UserGoal.php
+++ app/Models/UserGoal.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class UserGoal extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function sportTypes(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            StravaSportType::class,
+            'user_goal_activity_types',
+            'user_goal_id',
+            'type_id',
+        );
+    }
 }
