810a5969c00cf419446e49404911028c0d8b41a1
Coen <coenonderwater@gmail.com>
2022-11-30 22:55:44 +0100

    feat(forms): add badge selector

---
+++ resources/js/Components/BadgeButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from 'vue';
+
+defineEmits(['click']);
+
+const props = defineProps({
+    sportType: {
+        type: String,
+        required: true,
+    },
+    selected: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const selectedClass = computed(() => {
+    return props.selected
+        ? 'text-indigo-100 bg-[#734b6d] border-[#734b6d]'
+        : 'text-[#734b6d] bg-white hover:bg-[#ebe0e9]';
+});
+</script>
+
+<template>
+    <span
+        class="cursor-pointer mx-1 inline-flex items-center justify-center px-3 py-2 text-xs font-bold leading-none border-2 rounded"
+        :class="selectedClass"
+        v-on:click="$emit('click', sportType)">
+        {{ sportType }}
+    </span>
+</template>
