ae8a1b3bdc1f27fb78ae2954ebbb10260a884b6d
Coen <coenonderwater@gmail.com>
2022-11-30 22:56:56 +0100

    feat(user-goals): add getter for selected sport types in store request

--- app/Http/Requests/StoreUserGoalRequest.php
+++ app/Http/Requests/StoreUserGoalRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Models\StravaSportType;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Carbon;
 
@@ -19,7 +21,8 @@ class StoreUserGoalRequest extends FormRequest
         return [
             'goalTitle' => 'required',
             'goalStart' => 'required',
-            'goalEnd' => 'required'
+            'goalEnd' => 'required',
+            'selectedSportTypes' => 'required'
         ];
     }
 
@@ -36,5 +39,10 @@ class StoreUserGoalRequest extends FormRequest
     public function end(): Carbon
     {
         return $this->date('goalEnd');
+    }
+
+    public function selectedSportTypes(): Collection
+    {
+        return StravaSportType::whereIn('type', $this->get('selectedSportTypes'))->get();
     }
 }
