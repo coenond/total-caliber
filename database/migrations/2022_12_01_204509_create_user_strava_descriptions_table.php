2425c1a9e6e8c578245c202dd2ddab3a3ccb80ff
Coen <coenonderwater@gmail.com>
2022-12-01 23:47:11 +0100

    feat(strava-description): add UserStravaDescription model and migration

---
+++ database/migrations/2022_12_01_204509_create_user_strava_descriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user_strava_descriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+
+            $table->boolean('totals')->default(true);
+            $table->boolean('week_stats')->default(true);
+            $table->boolean('month_stats')->default(true);
+
+            $table->timestamps();
+        });
+
+        Schema::table('user_strava_descriptions', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user_strava_descriptions');
+    }
+};
