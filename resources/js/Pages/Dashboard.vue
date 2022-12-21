90362eed3f8ac9d516177ee72b73ae930467162d
Coen <coenonderwater@gmail.com>
2022-12-21 23:18:01 +0100

    feat: update chart with correct data

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,25 +1,54 @@
 <script setup>
+import { ref, computed, onMounted } from 'vue';
 import StravaBtn from '@/Components/StravaBtn.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, Colors } from 'chart.js';
+import BadgeButton from '@/Components/BadgeButton.vue';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
-
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, Colors);
 
 const props = defineProps({
-    weekDataChart: { type: Array, required: false },
+    weekDataChartDataInTime: { type: Array, required: false },
+    weekDataChartDataInDistance: { type: Array, required: false },
+    weekDataChartLabels: { type: Array, required: false },
 });
 
+const weekChartOptions = computed(() => {
+    return {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top'
+            }
+        },
+        scales: {
+            y: {
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, ticks) {
+                        return showWeekChartIn.value === 'distance'
+                            ? value + 'km'
+                            : Math.floor(value / 60) + 'min';
+                    }
+                }
+            }
+        }
+    };
+});
 
-const weekChartOptions = {  
-    responsive: true
-};
-const weekChartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    datasets: props.weekDataChart
-};
+const showWeekChartIn = ref('distance');
+const weekChartDataComputed = computed(() => {
+    const data = showWeekChartIn.value === 'distance'
+        ? props.weekDataChartDataInDistance
+        : props.weekDataChartDataInTime;
+    return {
+        labels: props.weekDataChartLabels,
+        datasets: data
+    };
+});
+const toggleYearTotals = (type) => showWeekChartIn.value = type;
 </script>
 
 <template>
@@ -54,12 +83,18 @@ const weekChartData = {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-8">
                 <div class="overflow-hidden shadow-sm sm:rounded-lg p-6 bg-white border-b border-gray-200">
                     <h2 class="text-xl pb-10">
-                       Your year overview per week
+                       Your year overview per month
                     </h2>
+                    
+                    <div class="mb-5">
+                        <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">Show chart in: </label>
+                        <BadgeButton class="border-b mt-2" type="Distance" :selected="showWeekChartIn === 'distance'" @click="toggleYearTotals('distance')" />
+                        <BadgeButton class="border-b mt-2" type="Time" :selected="showWeekChartIn === 'time'" @click="toggleYearTotals('time')" />
+                    </div>
                     <Bar
                         id="weekly"
                         :options="weekChartOptions"
-                        :data="weekChartData"
+                        :data="weekChartDataComputed"
                     />
                 </div>
             </div>
