356e73987335dd89123ef6e0d5fc5db21fc8da16
Coen <coenonderwater@gmail.com>
2023-01-05 00:52:32 +0100

    feat(onboarding): return new onboarding pages

---
+++ app/Http/Controllers/OnboardingController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\StravaSportTypeEnum;
+use App\Jobs\SyncStravaActivities;
+use App\Models\StravaSyncJob;
+use App\Services\StravaAuthService;
+use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class OnboardingController extends Controller
+{
+    /** */
+    public function __construct(
+        private StravaAuthService $stravaAuthService
+    ) { }
+
+    public function index(Request $req): Response
+    {
+        $user = $req->user();
+        $userHasStravaAuth = $this->stravaAuthService->userHasStravaAuth($user);
+        return Inertia::render('Onboarding/Onboarding', [
+            'userHasStravaAuth' => $userHasStravaAuth,
+            'stravaAuthUrl' => $this->stravaAuthService->getAuthorizationUrl(true),
+            'syncIsOnCoolDown' => $user->hasSyncJobOnCoolDown(),
+        ]);
+    }
+
+    public function setGoal(Request $req): Response
+    {
+        $user = $req->user();
+        return Inertia::render('Onboarding/SetGoal', [
+            'sportTypeOptions' => StravaSportTypeEnum::supportedForGoals()
+        ]);
+    }
+
+
+    public function createSyncJob(Request $req): RedirectResponse
+    {
+        $user = $req->user();
+        $stravaJobModel = StravaSyncJob::create(['user_id' => $user->id]);
+
+        // SyncStravaActivities::dispatch($user, $stravaJobModel, 1, Carbon::now()->subYears(10), Carbon::now());
+
+        return Redirect::route('onboarding.setGoal')->with('message', 'We started syncing your activities in the background. This can take up to 5 minutes.');
+    }
+}
