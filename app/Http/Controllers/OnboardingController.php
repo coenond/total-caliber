018a7374590c25551293d42ffb1c4589a6911cf5
Coen <coenonderwater@gmail.com>
2023-01-07 00:33:55 +0100

    feat: implement controller functions for storing strava desc settings

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Enums\StravaSportTypeEnum;
+use App\Http\Requests\StoreUserGoalRequest;
+use App\Http\Requests\StoreUserStravaDescriptionRequest;
 use App\Jobs\SyncStravaActivities;
 use App\Models\StravaSyncJob;
 use App\Models\UserGoal;
+use App\Models\UserStravaDescription;
 use App\Services\StravaAuthService;
 use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
@@ -52,4 +55,60 @@ class OnboardingController extends Controller
 
         return Redirect::route('onboarding.setGoal')->with('message', 'We started syncing your activities in the background. This can take up to 5 minutes.');
     }
+
+    public function storeGoal(StoreUserGoalRequest $req)
+    {
+        $user = $req->user();
+        // @todo: Move to service
+        $userGoal = UserGoal::updateOrCreate([ 'user_id' => $user->id ], [
+            'name' => $req->name(),
+            'start' => $req->start()->toDateString(),
+            'end' => $req->end()->toDateString()
+        ]);
+        $userGoal->sportTypes()->sync($req->selectedSportTypes());
+
+        $message = $userGoal->create_at === $userGoal->updated_at
+            ? 'Your goal is created.'
+            : 'Goal is updated.';
+
+        return Redirect::route('onboarding.stravaDescription')->with('message', $message);
+    }
+
+    public function setStravaDescription(Request $req): Response
+    {
+        $user = $req->user();
+        $userGoal = UserGoal::whereUserId($user->id)->with('sportTypes')->first();
+        $stravaDescription = UserStravaDescription::whereUserId($user->id)->first();
+        return Inertia::render('Onboarding/StravaDescription', [
+            'sportTypeOptions' => StravaSportTypeEnum::supportedForGoals(),
+            'userGoal' => $userGoal,
+            'stravaDescription' => $stravaDescription,
+            'startReadable' => $userGoal->start->toFormattedDateString(),
+            'endReadable' => $userGoal->end->toFormattedDateString(),
+        ]);
+    }
+
+    public function storeStravaDescription(StoreUserStravaDescriptionRequest $req)
+    {
+        $user = $req->user();
+        // @todo: Move to service
+        UserStravaDescription::updateOrCreate([ 'user_id' => $user->id ], [
+            'enabled' => $req->enabled(),
+            'totals' => $req->showTotals(),
+            'week_stats' => $req->showWeekStats(),
+            'month_stats' => $req->showMonthStats()
+        ]);
+
+        $message = $req->enabled()
+            ? 'Strava description settings saved.'
+            : 'Strava description is disabled.';
+
+        return Redirect::route('onboarding.final')->with('message', $message);
+    }
+
+    public function finalPage(): Response
+    {
+        return Inertia::render('Onboarding/Final');
+    }
+
 }
