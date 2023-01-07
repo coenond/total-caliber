3bebc38a83d5a58e9e7eb57ad6670bccd237333b
Coen <coenonderwater@gmail.com>
2023-01-07 01:05:32 +0100

    chore: notification should always be on top

--- resources/js/Layouts/WavyLayout.vue
+++ resources/js/Layouts/WavyLayout.vue
@@ -29,7 +29,7 @@ onMounted(() => {
 </style>
 
 <template>
-    <div :class="position" class="transition-all fixed bottom-8 mx-8 flex p-4 bg-green-300 bg-opacity-80 rounded-lg lg:max-w-[400px] mx-auto py-6 px-4 sm:px-6 lg:px-8 lg:mt-4" role="alert">
+    <div :class="position" class="z-0 transition-all fixed bottom-8 mx-8 flex p-4 bg-green-300 bg-opacity-80 rounded-lg lg:max-w-[400px] mx-auto py-6 px-4 sm:px-6 lg:px-8 lg:mt-4" role="alert">
         <svg aria-hidden="true" class="relative flex-shrink-0 w-5 h-5 text-green-700 dark:text-green-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
         <span class="sr-only">Info</span>
         <div class="ml-3 text-m text-green-800">
