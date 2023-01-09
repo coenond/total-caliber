a2f126d092ecb73226a232b0ee5342f4fd50c9b3
Coen <coenonderwater@gmail.com>
2023-01-09 23:45:33 +0100

    feat: add strava connect button on settings page

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue';
 import { Link, Head } from '@inertiajs/inertia-vue3';
 import StravaBtn from '@/Components/StravaBtn.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
 
 
 const props = defineProps({
@@ -21,16 +20,18 @@ const closeModal = () => modalOpen.value = false;
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                My Profile
+                Profile Settings
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-sm sm:rounded-lg p-6 bg-white border-b border-gray-200">
-                    <h1 class="text-xl pb-10">
-                        Welcome <strong>{{ $page.props.auth.user.name }}</strong>,
-                    </h1>
+                    <h2 class="text-l pb-10 font-bold"> Manage Strava Connection </h2>
+                        <p>Click the button below to manage your Strava connection</p>
+                        <a :href=props.stravaAuthUrl>
+                            <StravaBtn />
+                        </a>
                 </div>
             </div>
 
@@ -69,7 +70,7 @@ const closeModal = () => modalOpen.value = false;
                 </div>
                 <!-- Modal footer -->
                 <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete all my data</button>
+                    <button type="button" disabled class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete all my data</button>
                     <button @click="closeModal()" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
                 </div>
             </div>
