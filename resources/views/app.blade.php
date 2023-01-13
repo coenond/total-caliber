9d8172ad20ab7d66db0095c03af3eb1a458880a6
Coen Onderwater <coenonderwater@gmail.com>
2023-01-13 22:17:03 +0100

    feat(ga): add tag manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-R43F258M0G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R43F258M0G');
+    </script>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
