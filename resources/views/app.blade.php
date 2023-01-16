7404aa8e4d86fa32dbec02c6b2a3bc72492a5032
Coen <coenonderwater@gmail.com>
2023-01-16 22:21:17 +0100

    chore: add boilerplate imports

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,14 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <!-- Font Awesome Icons -->
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <!-- Nucleo Icons -->
+        <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
+        <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
+        <!-- Popper -->
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
 
         <!-- Scripts -->
         @routes
