9e604e55d343f1631dc89f5f43133c7167104710
Coen <coenonderwater@gmail.com>
2023-12-05 22:09:08 +0100

    chore: add vue-apexchart lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,8 @@ module.exports = {
 
     plugins: [
         require('@tailwindcss/forms'),
-        require('flowbite/plugin')
+        require('flowbite/plugin')({
+            charts: true,
+        }),
     ],
 };
