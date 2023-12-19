7e89be6529991a3045a11168fa852675e241fcd2
Coen <coenonderwater@gmail.com>
2023-12-19 22:20:16 +0100

    chore: swap dashboard cards

--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -7,13 +7,13 @@
 
     <livewire:pulse.cache cols="4" />
 
-    <livewire:pulse.slow-queries cols="8" />
+    <livewire:pulse.slow-outgoing-requests cols="8" />
 
     <livewire:pulse.exceptions cols="6" />
 
-    <livewire:pulse.slow-requests cols="6" />
-
     <livewire:pulse.slow-jobs cols="6" />
 
-    <livewire:pulse.slow-outgoing-requests cols="6" />
+    <livewire:pulse.slow-requests cols="6" />
+
+    <livewire:pulse.slow-queries cols="6" />
 </x-pulse>
